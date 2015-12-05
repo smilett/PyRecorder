c55f307f822a950b3b43aceee1e288e369b589cb
smilett <cmxsmile@gmail.com>
2015-12-06 00:55:10 +0800

    modify code

--- code/pyrecorder.py
+++ code/pyrecorder.py
@@ -28,45 +28,53 @@ WAVFILE = '../wav/record.wav'
 class App:
     def __init__(self, parent):       
 
-        #------ constants for controlling layout ------
-        button_width = 6      ### (1)
+        ###############################################
+        #      constants for controlling layout       #
+        ###############################################
+        button_width = 6 
         
-        button_padx = "2m"    ### (2)
-        button_pady = "1m"    ### (2)
+        button_padx = "2m"
+        button_pady = "1m"
 
-        buttons_frame_padx =  "3m"   ### (3)
-        buttons_frame_pady =  "2m"   ### (3)        
-        buttons_frame_ipadx = "3m"   ### (3)
-        buttons_frame_ipady = "1m"   ### (3)
-        # -------------- end constants ----------------
+        buttons_frame_padx =  "3m"
+        buttons_frame_pady =  "2m"       
+        buttons_frame_ipadx = "3m"
+        buttons_frame_ipady = "1m"
 
-        self.myParent = parent 
-        parent.geometry("600x400")  ### (1) Note geometry Window Manager method
+        ##############################
+        #      set window size       #
+        ##############################
+        self.myParent = parent
+        self.maximise()
 
         ### Our topmost frame is called myContainer1
-        self.myContainer1 = Frame(parent) ###
+        self.myContainer1 = Frame(parent)
         self.myContainer1.pack()
 
-        # -------------- message frame ----------------
+        ############################
+        #      message frame       #
+        ############################
         self.message_frame = Frame(self.myContainer1) ###
         self.message_frame.pack(side=TOP, expand=NO,  padx=10, pady=5, ipadx=5, ipady=5)
         
-        myMessage = "This is a recording programme"
+        myMessage = "This is a recording programme !\n"
         Label(self.message_frame, text = myMessage, justify = LEFT).pack(side = TOP, anchor = W)
 
-        # -------------- text frame ----------------
+        #########################
+        #      text frame       #
+        #########################
         self.text_frame = Frame(self.myContainer1, borderwidth=2,  relief=RIDGE, height=100, width=400, background="white")
-        self.text_frame.pack(side=TOP, fill=BOTH, expand=YES)
+        self.text_frame.pack(side = TOP, fill = BOTH, expand = YES)
 
-        # -------------- button frame ----------------
+        ###########################
+        #      button frame       #
+        ###########################
         self.buttons_frame = Frame(self.myContainer1)
-        self.buttons_frame.pack(
-            side = TOP, ipadx = buttons_frame_ipadx, ipady = buttons_frame_ipady, padx = buttons_frame_padx, pady = buttons_frame_pady)                  
+        self.buttons_frame.pack(side = TOP, ipadx = buttons_frame_ipadx, ipady = buttons_frame_ipady, padx = buttons_frame_padx, pady = buttons_frame_pady)                  
 
         # record button
         self.button_record = Button(self.buttons_frame, command = self.button_record_Click)
         self.button_record.configure(text = "Record")
-        self.button_record.focus_force()       
         self.button_record.configure(width = button_width, padx = button_padx, pady = button_pady)
         self.button_record.pack(side=LEFT)
         
@@ -74,6 +82,7 @@ class App:
         self.button_play = Button(self.buttons_frame, command = self.button_play_Click)
         self.button_play.configure(text = "Play")  
         self.button_play.configure(width = button_width, padx = button_padx, pady = button_pady)
+        self.button_play.configure(state = DISABLED) 
         self.button_play.pack(side = LEFT)
 
         # quit button
@@ -81,16 +90,43 @@ class App:
         self.button_quit.configure(text = "Quit")
         self.button_quit.configure(width = button_width, padx = button_padx, pady = button_pady)
         self.button_quit.pack(side = LEFT)
-                          
+        
+        #########################
+        #      foot frame       #
+        #########################
+        self.foot_frame = Frame(self.myContainer1) ###
+        self.foot_frame.pack(side = TOP, fill = BOTH, expand = YES)
+        
+        ### author_frame 
+        self.author_frame = Frame(self.foot_frame, borderwidth = 0,  relief=  RIDGE, width = 200) 
+        self.author_frame.pack(side = LEFT, fill = BOTH, expand = YES)  
+        myMessage_author = "Author: Mengxue Cao"
+        Label(self.author_frame, text = myMessage_author, justify = LEFT).pack(side = LEFT, anchor = W)
+
+        ### version_frame 
+        self.version_frame = Frame(self.foot_frame, borderwidth = 0,  relief = RIDGE, width = 200)
+        self.version_frame.pack(side = RIGHT,fill = BOTH, expand = YES)
+        myMessage_version = "Version: 0.1"
+        Label(self.version_frame, text = myMessage_version, justify = RIGHT).pack(side = RIGHT, anchor = W)
         
     def button_record_Click(self):
         record_wave()
+        self.button_play.configure(state = NORMAL) 
 
     def button_play_Click(self):
         play_wave() 
 
     def button_quit_Click(self):
-        self.myParent.destroy() 
+        self.myParent.destroy()
+
+    def maximise (self):
+        w, h = self.myParent.winfo_screenwidth(), self.myParent.winfo_screenheight()
+        width = w * 0.6
+        hight = h * 0.6
+        left_blank = (w - width) / 2
+        top_blank = (h - hight) / 2
+        self.myParent.geometry("%dx%d+%d+%d" % (width, hight, left_blank, top_blank))
+
 
 
 ##################
@@ -111,11 +147,7 @@ def record_wave():
     #open the input of wave
     wav_in = PyAudio()
 
-    stream = wav_in.open(format = FORMAT,
-                     channels = CHANNELS,
-                     rate = RATE,
-                     input = True,
-                     frames_per_buffer = CHUNK)
+    stream = wav_in.open(format = FORMAT, channels = CHANNELS, rate = RATE, input = True, frames_per_buffer = CHUNK)
     
     print("* recording")
 
@@ -127,8 +159,6 @@ def record_wave():
         string_audio_data = stream.read(CHUNK)
         save_buffer.append(string_audio_data)        
         print '.'
-        #plt.plot(count, float(string_audio_data))
-        #plt.show()
         count += 1
     
     sampwidth = wav_in.get_sample_size(FORMAT)
@@ -145,7 +175,6 @@ def record_wave():
 def play_wave():
     wf = wave.open(WAVFILE, 'rb')
 
-    # instantiate PyAudio (1)
     wav_out = PyAudio()
 
     # open stream (2)
@@ -153,21 +182,17 @@ def play_wave():
                 channels = wf.getnchannels(),
                 rate = wf.getframerate(),
                 output = True)
-    # read data
-    data = wf.readframes(CHUNK)
+    
+    data = wf.readframes(CHUNK) # read data
 
     # play stream (3)
     while len(data) > 0:
         stream.write(data)
         data = wf.readframes(CHUNK)
 
-    # stop stream (4)
     stream.stop_stream()
     stream.close()
-
-    # close PyAudio (5)
-    wav_out.terminate()
-
+    wav_out.terminate() 
 
 ##################
 #                #
